dbef8bd5a0bd0623555129ea77067cce74af7f53
Vipin <vipins110@gmail.com>
2019-02-07 11:27:20 +0530

    Removed unwanted const declarations

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -2,17 +2,17 @@ import qs from 'qs';
 
 const CLIENT_ID = '326ca3cfd1dec84';
 const CLIENT_SECRET = '88b25c522f94d721e8089aec66bcac90ffd6140a';
-const CALLBACK_URL = 'http://localhost:8080/imgur/callback';
+//const CALLBACK_URL = 'http://localhost:8080/imgur/callback';
 const ROOT_URL = 'http://api.imgur.com';
 
 export default {
 
     login(){
-        const queryString = {
+        const querystring = {
             client_id : CLIENT_ID,
             response_type : 'token'
         }
 
-        window.location = '${ROOT_URL}/oauth2/authorize?${qs.stringify(queryString)}';
+        window.location = ROOT_URL + '/oauth2/authorize?' + qs.stringify(querystring);
     }
 }
