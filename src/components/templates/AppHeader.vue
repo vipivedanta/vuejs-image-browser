1e2b8b591a2bde5803e70d56c704964af749902a
Vipin <vipins110@gmail.com>
2019-02-07 10:51:56 +0530

    Created Auth module inside components/modules and connected api to vuex main index.js file

---
+++ src/components/templates/AppHeader.vue
@@ -0,0 +1,33 @@
+<template>
+   <div class="row app-header">
+       <div class="col-md-3">
+           <h4>Image Browser</h4>
+       </div>
+
+       <div class="col-md-9">
+            <nav class="navbar navbar-expand-lg">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+               
+              </ul>
+            </div>
+</nav>
+       </div>
+   </div> 
+</template>
+
+<script>
+export default {
+    name : 'AppHeader'
+}
+</script>
+
+<style>
+.app-header{
+    background-color: antiquewhite;
+    padding: 10px;
+}
+</style>
