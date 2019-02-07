906fe8a7ee30678c78a472cd6aa897ca1ce30011
Vipin <vipins110@gmail.com>
2019-02-07 12:22:18 +0530

    Removed unwanted const declarations from imgur api

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,8 +1,7 @@
 import qs from 'qs';
 
 const CLIENT_ID = '326ca3cfd1dec84';
-const CLIENT_SECRET = '88b25c522f94d721e8089aec66bcac90ffd6140a';
-//const CALLBACK_URL = 'http://localhost:8080/imgur/callback';
+//const CLIENT_SECRET = '88b25c522f94d721e8089aec66bcac90ffd6140a';
 const ROOT_URL = 'http://api.imgur.com';
 
 export default {
